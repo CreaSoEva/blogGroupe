a7468cb05c2483fd9ea1c92321877c6ff1f582b7
Daemons03 <32901190+Daemons03@users.noreply.github.com>
2017-11-17 20:35:42 +0100

    premier import de css

--- index.php
+++ index.php
@@ -8,14 +8,81 @@
 	<script type="text/javascript" src="../js/script.js"></script>
 </head>
 <body>
+	<!-- Header -->
 	<header>
-		<img src="../image/banniere.jpg">
+		<img src="../image/banniere.png">
 		<p>Bonjour <span>Maxime</span><br/><br/>
 		<button>Déconnexion</button></p>
 	</header>
+	<!-- Fin de header -->
+	<!-- Aside -->
 	<aside>
-		
+		<p class="gras">Écrire un nouvel article</p>
+		<hr color="black">
+		<p class="gras">Catégorie:</p>
+		<ul>
+			<li>- Alcool fort</li>
+			<li>- Vin</li>
+			<li>- Champagne</li>
+		</ul>
+		<hr color="black">
+		<p class="gras">Derniers articles parus:</p>
+		<ul>
+			<li>- Mercurey par Maxime</li>
+			<li>- Lanson par Sébastien</li>
+			<li>-Whisky par Sophie</li>
+		</ul>
+		<hr color="black">
+		<p class="gras">Derniers commentaires:</p>
+		<ul>
+			<li>- Sophie sur Lanson</li>
+			<li>- Maxime sur Whisky</li>
+		</ul>
+		<hr color="black">
+		<p class="gras">Mes articles:</p>
+		<ul>
+			<li>- Whisky</li>
+		</ul>
 	</aside>
+	<!-- Fin de aside -->
+	<!-- Section -->
+	<section>
+			<h1>Derniers articles</h1>
+		<article>
+			<p>Catégorie: Vin</p>
+			<h3>MERCUREY</h3>
+			<p>Par Maxime le 17/11/2017</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<button class="inline">Modifier</button><button class="inline">Supprimer</button>
+		</article>
+		<article>
+			<p>Catégorie: Champagne</p>
+			<h3>Lanson</h3>
+			<p>Par Sébastien le 17/11/2017</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<button class="inline">Modifier</button><button class="inline">Supprimer</button>
+		</article>
+	</section>
 
 
 
