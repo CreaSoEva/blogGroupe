62ad1e55d846edfb6a5fe997dcbf402358abf331
M.Tyrans <maximedu03120@hotmail.fr>
2017-11-17 16:53:03 +0100

    debut css

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blog  des alcooliques</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="../js/script.js"></script>
+</head>
+<body>
+	<header>
+		<img src="../image/banniere.jpg">
+		<p>Bonjour <span>Maxime</span><br/><br/>
+		<button>Déconnexion</button></p>
+	</header>
+	<aside>
+		
+	</aside>
+
+
+
+
+</body>
+</html>
