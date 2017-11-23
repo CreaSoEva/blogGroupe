d45cce070d760a6ba1d23e838b4d865e6ab100b1
M.Tyrans <maximedu03120@hotmail.fr>
2017-11-23 11:48:16 +0100

    efef

---
+++ inc/headernonconnec.inc.php
@@ -0,0 +1,13 @@
+<header>
+		<a href="?page="><img src="./image/banniere.png" class="imageheaderG"></a>
+		<a href="?page="><img src="./image/logo.png" class="imageheaderP"></a>
+		<ul>
+			<form method="POST" action="?page=connexion">
+			<li><input type="email" name="email" placeholder="Email"></li>
+			<li><input type="password" name="password" placeholder="Password"></li>
+			<li><button>Connexion</button></li><br/>
+			</form>
+			<li>Pas encore inscrit?</li>
+			<li><button><a href="?page=inscription">Inscription</a></button></li>
+		</ul>
+</header>
